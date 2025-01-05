d328a4ee42433c93f92ae37a0ca844fb9016b6d2
MohammedAbdalrzzaq <mohammedabdalrzzaq2@gmail.com>
2025-01-05 22:42:35 +0300

    may
    may

---
+++ lp.js
@@ -0,0 +1,4 @@
+document.getElementById('menuButton').addEventListener('click', function() {
+    const dropdownMenu = document.getElementById('dropdownMenu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+});
